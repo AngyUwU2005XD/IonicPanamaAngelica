e4ff63ecfb3b94bf68714e6a8ba5d9267d20c377
Angelica Panama <angelica.panamar.est@uets.edu.ec>
2023-02-13 11:47:46 -0500

    primer ionic

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['style.css']
+})
+export class Tab3Page {
+
+  constructor(private tab3: AlertController) {}
+  async presentAlert() {
+    const alert = await this.tab3.create({
+      header: 'Please enter your info',
+      buttons: ['Cancel', 'OK'],
+      inputs: [
+        {
+          placeholder: 'Name',
+        },
+        {
+          placeholder: 'Nickname (max 8 characters)',
+          attributes: {
+            maxlength: 8,
+          },
+        },
+        {
+          type: 'number',
+          placeholder: 'Age',
+          min: 1,
+          max: 100,
+        },
+        {
+          type: 'textarea',
+          placeholder: 'A little about yourself',
+        },
+      ],
+    });
+
+    await alert.present();
+  }
+}
+
+
